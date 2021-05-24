703d09be98b96867faf79f04b0504b85e5475c79
Maxi Prez <maxi.prez@gmail.com>
2021-05-24 15:04:30 -0300

    R3 with responsive modal added

--- R3.js
+++ R3.js
@@ -2,11 +2,20 @@ const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
+//Evento que abre el modal
 open.addEventListener('click', () => {
     modal_container.classList.add('show');
     // console.log('hiciste clic');
 })
 
+//Evento para cerrar el modal
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 })
+
+//Evento que cierra el modal cuando se hace click afuera
+window.onclick = function(event) {
+    if (event.target == modal_container) {
+        modal_container.classList.remove('show');
+    }
+  }
--- R4.js
+++ R4.js
@@ -1,14 +1,16 @@
 const url = 'https://rickandmortyapi.com/api/character'
 
+// Llamada desde fetch a la url
     fetch(url)
-    .then(res => res.json())
-    .then(data => mostrarData(data))
-    .catch(err => console.log(err))
+    .then(res => res.json()) //Promesa
+    .then(data => mostrarData(data)) //Devuelve la promesa
+    .catch(err => console.log(err)) //Devuelve error por consola
 
     const mostrarData = (data) => {
         console.log(data.results) //Para test
         let body = ''
-
+        
+// Defino ciclo for para que pinte en la tabla c/u de los items
         for ( let i = 0; i<data.results.length; i++){
                 body += `
                 <tr>
